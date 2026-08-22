b0bbd3962d5cfe33c95797bf8652d91cb63ae94b
petermaisonwilson <peter@maisonwilson.fr>
2026-08-22 12:56:16 +0200

    Web V1: register full enquiry builder

--- online/webv1.py
+++ online/webv1.py
@@ -4,6 +4,7 @@ from .setup015 import register_setup015
 from .webv1_core import initialise_web_v1
 from .webv1_customers import register_customer_routes
 from .webv1_enquiries import register_enquiry_routes
+from .webv1_enquiry_builder import register_enquiry_builder_routes
 from .webv1_routes import register_web_v1_routes
 
 __all__ = ['initialise_web_v1', 'register_web_v1']
@@ -16,3 +17,4 @@ def register_web_v1(app) -> None:
     register_web_v1_routes(app)
     register_customer_routes(app)
     register_enquiry_routes(app)
+    register_enquiry_builder_routes(app)
