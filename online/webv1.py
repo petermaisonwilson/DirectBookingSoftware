0e5abd7aa0c1bcf4b68314e18cc3d996efc7a5ba
petermaisonwilson <peter@maisonwilson.fr>
2026-08-22 10:47:05 +0200

    Web V1: register Client Register routes

--- online/webv1.py
+++ online/webv1.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .setup015 import register_setup015
 from .webv1_core import initialise_web_v1
+from .webv1_customers import register_customer_routes
 from .webv1_routes import register_web_v1_routes
 
 __all__ = ['initialise_web_v1', 'register_web_v1']
@@ -12,3 +13,4 @@ def register_web_v1(app) -> None:
     register_setup015(app)
     initialise_web_v1(app.state.database)
     register_web_v1_routes(app)
+    register_customer_routes(app)
