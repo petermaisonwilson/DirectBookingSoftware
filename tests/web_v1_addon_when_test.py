4c933b9a3464b3e1372069f5a1ef8446deec0ce5
petermaisonwilson <peter@maisonwilson.fr>
2026-08-22 16:53:20 +0200

    Use standard project path setup in Add-on When test

--- tests/web_v1_addon_when_test.py
+++ tests/web_v1_addon_when_test.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import sys
 import tempfile
 from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from fastapi.testclient import TestClient
 
