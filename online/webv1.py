dd42758d6c2202a5e9e689f48fc224da637fe98f
petermaisonwilson <peter@maisonwilson.fr>
2026-08-22 12:21:38 +0200

    Web V1: register Enquiry workspace

--- online/webv1.py
+++ online/webv1.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .setup015 import register_setup015
 from .webv1_core import initialise_web_v1
 from .webv1_customers import register_customer_routes
+from .webv1_enquiries import register_enquiry_routes
 from .webv1_routes import register_web_v1_routes
 
 __all__ = ['initialise_web_v1', 'register_web_v1']
@@ -14,3 +15,4 @@ def register_web_v1(app) -> None:
     initialise_web_v1(app.state.database)
     register_web_v1_routes(app)
     register_customer_routes(app)
+    register_enquiry_routes(app)
